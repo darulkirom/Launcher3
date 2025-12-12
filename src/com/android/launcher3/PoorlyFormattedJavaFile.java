5a0daee238673fc58e31e0be376b46bd27394a06
Adnan Begovic <abegovic@google.com>
2025-12-11 16:05:14 -0800

    Add poorly formatted files to test presubmit checks.
    These files are intentionally misformatted to verify that the presubmit checks for code style are functioning correctly for both Java and Kotlin.
    
    Change-Id: I6098bfdab6eec43b2350ea71896b905da8f50f43

---
+++ src/com/android/launcher3/PoorlyFormattedJavaFile.java
@@ -0,0 +1,10 @@
+package com.android.launcher3;
+
+/**
+ * This is a poorly formatted Java file. It is used to test the presubmit checks.
+ */
+public class PoorlyFormattedJavaFile {
+    public void test() {
+    System.out.println("Hello World");
+  }
+}
---
+++ src/com/android/launcher3/PoorlyFormattedKotlinFile.kt
@@ -0,0 +1,10 @@
+package com.android.launcher3
+
+/**
+ * This is a poorly formatted Kotlin file. It is used to test the presubmit checks.
+ */
+class PoorlyFormattedKotlinFile {
+fun test() {
+    println("Hello World")
+        }
+}
